322634914c5b445378fa9a50e0eeea4aac389f56
Mafifiza <168578665+Mafifiza@users.noreply.github.com>
2024-10-06 14:39:27 +0200

    Added the basics of JavaScript

--- src/index.js
+++ src/index.js
@@ -44,3 +44,15 @@ function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}$key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cape Town");
